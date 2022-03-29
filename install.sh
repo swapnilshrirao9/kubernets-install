fb4356f85165191e6dd97c24d2cc99b97e0dd34e
swapnilshrirao9 <67373271+swapnilshrirao9@users.noreply.github.com>
2022-03-29 16:56:15 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y apt-transport-https ca-certificates curl
 #Install the Google cloud package
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add-
 # Add repository to the /etc/apt/source.list.d
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+echo "deb  https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list
 # C group
 cat <<EOF | sudo tee /etc/docker/daemon.json
 { "exec-opts": ["native.cgroupdriver=systemd"],
