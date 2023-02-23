d366cc223532a9f5bff3a63d3b308a938612c5a8
swapnilshrirao9 <67373271+swapnilshrirao9@users.noreply.github.com>
2023-02-24 03:10:46 +0530

    command location change

--- mastersetup.sh
+++ mastersetup.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 sudo swapoff -a
 sudo apt-get update
-apt install -y docker.io
 sudo apt-get install -y apt-transport-https ca-certificates curl
 #Install the Google cloud package
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 # Add repository to the /etc/apt/source.list.d
 echo "deb  https://apt.kubernetes.io/ kubernetes-xenial main" >> /etc/apt/sources.list
+apt update && apt install -y kubeadm kubectl kubelet docker.io
+apt-mark hold kubectl kubeadm kubelet
 # C group
 cat <<EOF | sudo tee /etc/docker/daemon.json
 { "exec-opts": ["native.cgroupdriver=systemd"],
@@ -16,9 +17,7 @@ cat <<EOF | sudo tee /etc/docker/daemon.json
 "storage-driver": "overlay2"
 }
 EOF
-apt update && apt install -y kubeadm kubectl kubelet
-apt-mark hold kubectl kubeadm kubelet
-kubeadm init --pod-network-cidr=10.244.0.0/24
+kubeadm init --pod-network-cidr=10.244.0.0/24 > OUTPUT.txt
 #kubeadm token create --print-join-command
 kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
 mkdir -p $HOME/.kube
