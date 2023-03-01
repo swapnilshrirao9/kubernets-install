8a4bf93f7c76a2715e67221c2c565e1db968fb76
swapnilshrirao9 <67373271+swapnilshrirao9@users.noreply.github.com>
2023-03-01 20:11:28 +0530

    error update

--- error.sh
+++ error.sh
@@ -1,2 +1,10 @@
 #docker install 
 wget -qO- https://get.docker.com/ | sh
+ctr plugin ls
+kubeadm certs check-expiration
+kubeadm int phase certs front-proxy-ca
+kubeadm init phase certs ca 
+sudo systemctl daemon-reload
+#Error 
+sudo systemctl list-unit-files
+sudo systemctl unmask docker
