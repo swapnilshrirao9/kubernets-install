fa0ff841a3054f64700fa526f846632e29054247
swapnilshrirao9 <67373271+swapnilshrirao9@users.noreply.github.com>
2023-03-05 17:43:24 +0530

    error update

--- error.sh
+++ error.sh
@@ -8,3 +8,34 @@ sudo systemctl daemon-reload
 #Error 
 sudo systemctl list-unit-files
 sudo systemctl unmask docker
+ sudo systemctl stop docker
+  cp -au /var/lib/docker /var/lib/docker.bk
+  sudo systemctl start docker
+  137  sudo dockerd --debug
+  138  ps -aux | grep docker 
+  139  yum remove docker
+  140  yum autoremove
+  141  sudo systemctl start dockersudo yum install -y yum-utils
+  142  sudo yum install -y yum-utils
+  143  sudo yum-config-manager     --add-repo     https://download.docker.com/linux/centos/docker-ce.repo
+  144  sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+  145  sudo systemctl start docker
+  146  systemctl restart docker
+  147  systemctl restart kubelete
+  148  systemctl restart kubelet
+  149  systemctl status docker
+  150  systemctl status kubelet
+  151  systemctl enable kubelet
+  152  systemctl status kubelet
+  153  kubeadm init
+  154  kubeadm reset -f
+  155  systemctl status kubelet
+  156  systemctl enable kubelet
+  157  systemctl status kubelet
+  158  systemctl start kubelet
+  159  systemctl status kubelet
+  160  kubelet contents
+  161  systemctl stop kubelet
+  162  swapoff -a
+  163  systemctl start kubelet
+  164  systemctl status kubelet
