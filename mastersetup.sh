edf26effa7f1cfb911f5bfa63358397ebeb63848
swapnilshrirao9 <67373271+swapnilshrirao9@users.noreply.github.com>
2023-03-06 17:52:44 +0530

    calico update
    calico is the network

--- mastersetup.sh
+++ mastersetup.sh
@@ -23,3 +23,9 @@ kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+reset 
+kubeadm reset -f
+rm -r $HOME/.kube
+sudo kubeadm init --apiserver-advertise-address=182.168.0.100 --pod-network-cidr=192.168.0.0/16
+kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.25.0/manifests/tigera-operator.yaml
+
